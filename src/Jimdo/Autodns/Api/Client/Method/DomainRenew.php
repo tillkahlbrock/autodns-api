51ace1a0f9d0cd45b795a39976b0f3430a2f26e3
Till Kahlbrock <till.kahlbrock@gmx.com>
2013-07-26 12:14:44 +0200

    adds domain renew method

--- src/Jimdo/Autodns/Api/Client/Method/DomainRenew.php
+++ src/Jimdo/Autodns/Api/Client/Method/DomainRenew.php
@@ -6,8 +6,16 @@ use Jimdo\Autodns\Api\Client\Method;
 
 class DomainRenew implements Method
 {
-
     public function createRequest(array $requestData)
     {
+        return array(
+            'code'   => '0101003',
+            'domain' => array(
+                'name'                => $requestData['name'],
+                'payable'             => $requestData['payable'],
+                'period'              => $requestData['period'],
+                'remove_cancellation' => $requestData['remove_cancellation']
+            )
+        );
     }
 }
---
+++ test/Jimdo/Autodns/Api/Client/Method/DomainRenewTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use Jimdo\Autodns\Api\Client\Method\DomainRenew;
+
+class DomainRenewTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function itShouldGenerateTheRequest()
+    {
+        $domainName = 'some-domain.org';
+        $payableDate = '2012-12-24';
+        $period = 1;
+        $removeCancellation = 'yes';
+
+        $method = new DomainRenew();
+
+        $requestData = array(
+            'name' => $domainName,
+            'payable' => $payableDate,
+            'period' => $period,
+            'remove_cancellation' => $removeCancellation
+        );
+
+        $expectedRequest = array(
+            'code' => '0101003',
+            'domain' => array(
+                'name' => $domainName,
+                'payable' => $payableDate,
+                'period' => $period,
+                'remove_cancellation' => $removeCancellation
+            )
+        );
+
+        $request = $method->createRequest($requestData);
+
+        $this->assertEquals($expectedRequest, $request);
+    }
+}
