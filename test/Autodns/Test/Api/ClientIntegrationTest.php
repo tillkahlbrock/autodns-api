85f51e079d2c983de3f6a5972fbca7033cf5490c
Till Kahlbrock <till.kahlbrock@gmx.com>
2013-09-06 07:40:34 +0200

    refactores ClientIntegrationTest

--- test/Autodns/Test/Api/ClientIntegrationTest.php
+++ test/Autodns/Test/Api/ClientIntegrationTest.php
@@ -18,14 +18,49 @@ class ClientIntegrationTest extends TestCase
     /**
      * @test
      */
-    public function itShouldWork()
+    public function itShouldMakeADomainInquireListCall()
     {
-        $responseXml = $this->getResponseXml();
-        $response = $this->aStub('Buzz\Message\MessageInterface')->with('getContent', $responseXml);
-        $expectedRequest = $this->getExpectedRequestXml();
-        $expectedResult = $this->getExpectedResult();
+        $taskName = 'DomainInquireList';
+        $responseXml = $this->getResponseXml($taskName);
+        $expectedRequest = $this->getExpectedRequestXml($taskName);
+        $expectedResult = new Client\Response(
+            array(
+                'result' => array(
+                    'data' => array(
+                        'summary' => '2',
+                        'domain' => array(
+                            array(
+                                'owner' => array(
+                                    'user' => 'customer',
+                                    'context' => '4'
+                                ),
+                                'name' => 'example.com',
+                                'created' => '2005-06-16 16:47:50'
+                            ),
+                            array(
+                                'owner' => array(
+                                    'user' => 'customer2',
+                                    'context' => '4'
+                                ),
+                                'name' => 'example.com',
+                                'created' => '2005-06-16 16:47:50',
+                                'domainsafe' => 'true',
+                                'dnssec' => 'false'
+                            )
+                        )
+                    ),
+                    'status' => array(
+                        'text' => 'Domaindaten wurden erfolgreich ermittelt.',
+                        'type' => 'success',
+                        'code' => 'S0105'
+                    )
+                )
+            )
+        );
 
-        $sender = $this->aStub('Buzz\Browser')->with('post', $response)->build();
+        $fakeResponse = $this->aStub('Buzz\Message\MessageInterface')->with('getContent', $responseXml);
+
+        $sender = $this->aStub('Buzz\Browser')->with('post', $fakeResponse)->build();
         $sender
             ->expects($this->once())
             ->method('post')
@@ -33,139 +68,39 @@ class ClientIntegrationTest extends TestCase
 
         $client = $this->buildClient($sender);
 
-        $task = $this->buildTask();
+        $query = Query::build();
+        $query = $query->addOr(
+            $query->addAnd(
+                array('name', 'like', '*.at'),
+                array('created', 'lt', '2012-12-*')
+            ),
+            array('name', 'like', '*.de')
+        );
+
+        $task = Request\TaskBuilder::build('DomainListInquiry')
+            ->withView(array('offset' => 0, 'limit' => 20, 'children' => 0))
+            ->withKeys(array('created', 'payable'))
+            ->withQuery($query);
 
         $this->assertEquals($expectedResult, $client->call($task));
     }
 
     /**
+     * @param $taskName
      * @return string
      */
-    private function getResponseXml()
+    private function getResponseXml($taskName)
     {
-        $xml = <<<XML
-<?xml version="1.0" encoding="utf-8" ?>
-<response>
-    <result>
-        <data>
-            <summary>2</summary>
-            <domain>
-                <owner>
-                    <user>customer</user>
-                    <context>4</context>
-                </owner>
-                <name>example.com</name>
-                <created>2005-06-16 16:47:50</created>
-            </domain>
-            <domain>
-                <owner>
-                    <user>customer2</user>
-                    <context>4</context>
-                </owner>
-                <name>example.com</name>
-                <created>2005-06-16 16:47:50</created>
-                <domainsafe>true</domainsafe>
-                <dnssec>false</dnssec>
-            </domain>
-        </data>
-        <status>
-            <text>Domaindaten wurden erfolgreich ermittelt.</text>
-            <type>success</type>
-            <code>S0105</code>
-        </status>
-    </result>
-</response>
-XML;
-        return $xml;
+        return file_get_contents(__DIR__ . '/testData/' . $taskName . '_response.xml');
     }
 
     /**
-     * @return \Autodns\Api\Client\Response
-     */
-    private function getExpectedResult()
-    {
-        $expectedResult = array(
-            'result' => array(
-                'data' => array(
-                    'summary' => '2',
-                    'domain' => array(
-                        array(
-                            'owner' => array(
-                                'user' => 'customer',
-                                'context' => '4'
-                            ),
-                            'name' => 'example.com',
-                            'created' => '2005-06-16 16:47:50'
-                        ),
-                        array(
-                            'owner' => array(
-                                'user' => 'customer2',
-                                'context' => '4'
-                            ),
-                            'name' => 'example.com',
-                            'created' => '2005-06-16 16:47:50',
-                            'domainsafe' => 'true',
-                            'dnssec' => 'false'
-                        )
-                    )
-                ),
-                'status' => array(
-                    'text' => 'Domaindaten wurden erfolgreich ermittelt.',
-                    'type' => 'success',
-                    'code' => 'S0105'
-                )
-            )
-        );
-
-        return new Client\Response($expectedResult);
-    }
-
-    /**
+     * @param $taskName
      * @return string
      */
-    private function getExpectedRequestXml()
+    private function getExpectedRequestXml($taskName)
     {
-        $expectedRequest = <<<RequestXml
-<?xml version="1.0" encoding="utf-8"?>
-<request>
-  <auth>
-    <user>user</user>
-    <password>password</password>
-    <context>4</context>
-  </auth>
-  <task>
-    <code>0105</code>
-    <view>
-      <offset>0</offset>
-      <limit>20</limit>
-      <children>0</children>
-    </view>
-    <key>created</key>
-    <key>payable</key>
-    <where>
-      <or>
-        <and>
-          <key>name</key>
-          <operator>like</operator>
-          <value>*.at</value>
-        </and>
-        <and>
-          <key>created</key>
-          <operator>lt</operator>
-          <value>2012-12-*</value>
-        </and>
-      </or>
-      <or>
-        <key>name</key>
-        <operator>like</operator>
-        <value>*.de</value>
-      </or>
-    </where>
-  </task>
-</request>
-
-RequestXml;
-        return $expectedRequest;
+        return file_get_contents(__DIR__ . '/testData/' . $taskName . '_request.xml');
     }
 
     /**
@@ -183,27 +118,5 @@ RequestXml;
             new Info(self::SOME_URL, 'user', 'password', 4)
         );
         return $client;
-    }
-
-    /**
-     * @return Client\Request\Task
-     */
-    private function buildTask()
-    {
-        $query = Query::build();
-        $query = $query->addOr(
-            $query->addAnd(
-                array('name', 'like', '*.at'),
-                array('created', 'lt', '2012-12-*')
-            ),
-            array('name', 'like', '*.de')
-        );
-
-        $task = Request\TaskBuilder::build('DomainListInquiry')
-            ->withView(array('offset' => 0, 'limit' => 20, 'children' => 0))
-            ->withKeys(array('created', 'payable'))
-            ->withQuery($query);
-
-        return $task;
     }
 }
